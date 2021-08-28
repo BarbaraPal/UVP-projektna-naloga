7a118ac95f8e5a5866fc6f747b64170cdc33b84a
Barbara <pal.barbi@gmail.com>
2021-08-28 16:26:21 +0200

    popravljena koda za iskanje rešitve sudokuja in dodan razred uporabnik

--- model.py
+++ model.py
@@ -1,12 +1,43 @@
+import json
+
+class Uporabnik:
+    def __init__(self, uporabnisko_ime, geslo, matrika):
+        self.uporabnisko_ime = uporabnisko_ime
+        self.geslo = geslo
+        self.matrika = matrika
+
+    def preveri_geslo(self, geslo):
+        if self.geslo != geslo:
+            raise ValueError('Napačno geslo!')
+
+    def preveri_enakost_gesel(geslo1, geslo2):
+        if geslo1 != geslo2:
+            raise ValueError('Gesli se ne ujemata!')
+
+    def shrani_matriko(self, ime_datoteke):
+        slovar_podatkov = {
+            'uporabnisko_ime': self.uporabnisko_ime,
+            'geslo': self.geslo,
+            'matrika': self.matrika,
+        }
+        with open(ime_datoteke, 'w') as datoteka:
+            json.dump(slovar_podatkov, datoteka, ensure_ascii=False, indent=4)
+  
+    def nalozi_matriko(ime_datoteke):
+        with open(ime_datoteke) as datoteka:
+            slovar_podatkov = json.load(datoteka)
+        uporabnisko_ime = slovar_podatkov['uporabnisko_ime']
+        geslo = slovar_podatkov['geslo']
+        matrika = slovar_podatkov['matrika']
+        uporabnik = cls.uporabnisko_ime, cls.geslo, cls.matrika
+        return uporabnik
+
+
 class Matrika:
     def __init__(self, matrika):
         self.matrika = [line[:] for line in matrika] 
         self.visina = len(self.matrika)
         self.sirina = len(self.matrika[0])
-
-    def __eq__(self, other):
-        '''Preveri ali sta matriki enaki.'''
-        return isinstance(other, Matrika) and self.matrika == other.matrika
 
     def __str__(self):
         return "\n".join(
@@ -32,144 +63,63 @@ class Matrika:
             self.matrika[vrstica - 1][stolpec - 1] = stevilo
 
     def odstrani_stevilo(self, vrstica, stolpec):
-        '''Odstrani število v matriki in ga nadomesti z vrednostjo None.'''
+        '''Odstrani število v matriki in ga nadomesti z vrednostjo 0.'''
         if str(vrstica) not in '123456789' or str(stolpec) not in '123456789':
             raise ValueError('Prosimo, da za vrstice, stolpce in števila izbirate števila od 1 do 9.')
         else:
             vrstica = int(vrstica)
             stolpec = int(stolpec)
-        if self.matrika[vrstica - 1][stolpec - 1] == None:
+        if self.matrika[vrstica - 1][stolpec - 1] == 0:
             pass
         elif vrstica > 9 or vrstica < 1:
             raise ValueError('Ta vrstica ne obstaja.')
         elif stolpec > 9 or stolpec < 1:
             raise ValueError('Ta stolpec ne obstaja.')
         else:
-            self.matrika[vrstica - 1][stolpec - 1] = None
+            self.matrika[vrstica - 1][stolpec - 1] = 0
 
     def izbrisi_cel_sudoku(self):
-        self.matrika = [9 * [None], 9 * [None], 9 * [None], 9 * [None], 9 * [None], 9 * [None], 9 * [None], 9 * [None], 9 * [None]]
+        self.matrika = [9 * [0], 9 * [0], 9 * [0], 9 * [0], 9 * [0], 9 * [0], 9 * [0], 9 * [0], 9 * [0]]
         return self.matrika
 
-    def preveri_ustreznost_vrstic(self):
-        '''Preveri če v nobeni vrstici katero od števil ni zapisano več kot enkrat.'''
-        for i in range(9):
-            for j in range(9):
-                for k in range(9):
-                    if self.matrika[i][j] == self.matrika[i][k]:
-                        return 'Prosim, da ustrezno vnesete števila. V isti vrstici ne sme biti večkrat napisano enako število.'
-        return True
+    def najdi_naslednjo_prazno_celico(self, vrstica, stolpec):
+        for i in range(vrstica - 1, 9):
+            for j in range(stolpec - 1, 9):
+                if self.matrika[i][j] == 0:
+                    return i, j
+        for i in range(0,9):
+            for j in range(0,9):
+                if self.matrika[i][j] == 0:
+                    return i, j
+        return -1, -1
 
-    def preveri_ustreznost_stolpcev(self):
-        '''Preveri če v nobenem stolpcu katero od števil ni zapisano več kot enkrat.'''
-        for j in range(9):
-            for i in range(9):
-                for k in range(9):
-                    if self.matrika[i][j] == self.matrika[k][j]:
-                        return 'Prosim, da ustrezno vnesete števila. V istem stolpcu ne sme biti večkrat napisano enako število.'
-        return True
+    def preveri_ustreznost(self, vrstica, stolpec, stevilo):
+        vrsticaok = all([stevilo != self.matrika[vrstica - 1][j] for j in range(9)])
+        if vrsticaok:
+            stolpecok = all([stevilo != self.matrika[i][stolpec - 1] for i in range(9)])
+            if stolpecok:
+                vrst, stol = 3 * ((vrstica - 1) // 3), 3 * ((stolpec - 1) // 3)
+                for i in range(vrst, vrst + 3):
+                    for j in range(stol, stol + 3):
+                        if self.matrika[i][j] == stevilo:
+                            return False
+                return True
+        return False
 
-    def preveri_ustreznost_kvadratov(self):
-        '''Preveri če v nobenem kvadratu 3x3 katero od števil ni zapisano več kot enkrat.'''
-        for i in range(3):
-            for k in range(3):
-                for j in range(3):
-                    for l in range(3):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'    
-                for j in range(3,6):
-                    for l in range(3,6):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'
-                for j in range(6,9):
-                    for l in range(6,9):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'
-        for i in range(3,6):
-            for k in range(3,6):
-                for j in range(3):
-                    for l in range(3):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'    
-                for j in range(3,6):
-                    for l in range(3,6):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'
-                for j in range(6,9):
-                    for l in range(6,9):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'
-        for i in range(6,9):
-            for k in range(6,9):
-                for j in range(3):
-                    for l in range(3):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'    
-                for j in range(3,6):
-                    for l in range(3,6):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'
-                for j in range(6,9):
-                    for l in range(6,9):
-                        if self.matrika[i][j] == self.matrika[k][l]:
-                            return 'Prosim, da ustrezno vnesete števila.'
-        return True
-
-    def preveri_ustreznost(self):
-        '''Preveri ustreznost kvadratov, vrstic in stolpcev naenkrat.'''
-        return preveri_ustreznost_kvadratov(self) == True and preveri_ustreznost_stolpcev(self) == True and preveri_ustreznost_vrstic(self) == True 
-
-    def preveri_ali_je_dovolj_stevil(self):
-        '''Preveri ali je v začetku vnesenih dovolj števil.'''
-        vsota = 0
-        for i in self.matrika:
-            for j in i:
-                if i != None:
-                    vsota += 1
-        if vsota > 16:
+    def resevanje(self, vrstica=1, stolpec=1):
+        vrstica, stolpec = self.najdi_naslednjo_prazno_celico(vrstica, stolpec)
+        if vrstica == -1:
             return True
-
-    def preveri_ali_je_dovolj_razlicnih_stevil(self):
-        '''Preveri ali je v začetku vnesenih dovolj različnih števil.'''
-        seznam = []
-        for i in self.matrika:
-            if i != None and i not in seznam:
-                seznam += [i]
-        if len(seznam) >= 8:
-            return True
-
-    def iskanje_matrike_vseh_moznosti(self):
-        '''Za vsako prazno polje v sudokuju najde vse možne rešitve.'''
-        matrika_vseh_moznosti = self.matrika
-        for i in range(9):
-            for j in range(9):
-                if self.matrika[i][j] == None:
-                    for k in range(1,10):
-                        self.matrika[i][j] = k
-                        if preveri_ustreznost(self):
-                            matrika_vseh_moznosti[i][j] += [k]
-        return matrika_vseh_moznosti
-
-
-    def resevanje(self):
-        matrika_vseh_moznosti = iskanje_matrike_vseh_moznosti(self)
-        for i in range(9):
-            for j in range(9):
-                for k in range(len(matrika_vseh_moznosti[i][j])):
-                    if len(matrika_vseh_moznosti[i][j]) == 1:
-                        pass
-                    else:
-                        self.matrika[i][j] = matrika_vseh_moznosti[i][j][k]
-                        matrika_vseh_moznosti[i][j] -= matrika_vseh_moznosti[i][j][k]
-
+        for stevilo in range(1,10):
+            if self.preveri_ustreznost(vrstica, stolpec, stevilo):
+                self.matrika[vrstica - 1][stolpec - 1] = stevilo
+                if self.resevanje(vrstica, stolpec):
+                    return True
+                self.matrika[vrstica - 1][stolpec - 1] = 0
+        return False
 
     def resi(self):
-        if preveri_ali_je_dovolj_stevil(self) != True:
-            raise ValueError('Prosim vnesite vsaj 16 števil.')
-        elif preveri_ali_je_dovolj_razlicnih_stevil(self) != True:
-            raise ValueError('Prosim vnesite dovolj različnih števil od 1 do 9.')
-        elif preveri_ustreznost(self) != True:
-            pass
+        if self.resevanje() == False:
+            raise ValueError('Sudokuja ni mogoče rešiti. Preverite ali ste pravilno vnesli vsa števila.')
         else:
-            resevanje(self)
-    
+            return self.matrika
