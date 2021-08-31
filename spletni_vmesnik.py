cedb5ecd3e27fd7adb44143c0057e5557e9d071e
Barbara <pal.barbi@gmail.com>
2021-09-01 00:11:43 +0200

    manjši popravki

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -3,6 +3,7 @@ import hashlib
 import os
 from model import Uporabnik, Matrika
 
+
 @bottle.get("/")
 def zacetna_stran():
     bottle.redirect("/matrika/")
@@ -10,22 +11,27 @@ def zacetna_stran():
 @bottle.get("/matrika/")
 def prikaz_matrike():
     return bottle.template('matrika.html')
+    
+@bottle.get("/resen_sudoku/")
+def prikaz_resenega():
+    return bottle.template('resen_sudoku.html')
 
 @bottle.post("/resen_sudoku/")
 def resevanje_sudokuja():
     matrika = [
-        [bottle.request.query('11')), bottle.BaseRequest.query('12'), bottle.BaseRequest.query('13'), bottle.BaseRequest.query('14'), bottle.BaseRequest.query('15'), bottle.BaseRequest.query('16'), bottle.BaseRequest.query('17'), bottle.BaseRequest.query('18'), bottle.BaseRequest.query('19')],
-        [bottle.BaseRequest.query('21'), bottle.BaseRequest.query('22'), bottle.BaseRequest.query('23'), bottle.BaseRequest.query('24'), bottle.BaseRequest.query('25'), bottle.BaseRequest.query('26'), bottle.BaseRequest.query('27'), bottle.BaseRequest.query('28'), bottle.BaseRequest.query('29')],
-        [bottle.BaseRequest.query('31'), bottle.BaseRequest.query('32'), bottle.BaseRequest.query('33'), bottle.BaseRequest.query('34'), bottle.BaseRequest.query('35'), bottle.BaseRequest.query('36'), bottle.BaseRequest.query('37'), bottle.BaseRequest.query('38'), bottle.BaseRequest.query('39')],
-        [bottle.BaseRequest.query('41'), bottle.BaseRequest.query('42'), bottle.BaseRequest.query('43'), bottle.BaseRequest.query('44'), bottle.BaseRequest.query('45'), bottle.BaseRequest.query('46'), bottle.BaseRequest.query('47'), bottle.BaseRequest.query('48'), bottle.BaseRequest.query('49')],
-        [bottle.BaseRequest.query('51'), bottle.BaseRequest.query('52'), bottle.BaseRequest.query('53'), bottle.BaseRequest.query('54'), bottle.BaseRequest.query('55'), bottle.BaseRequest.query('56'), bottle.BaseRequest.query('57'), bottle.BaseRequest.query('58'), bottle.BaseRequest.query('59')],
-        [bottle.BaseRequest.query('61'), bottle.BaseRequest.query('62'), bottle.BaseRequest.query('63'), bottle.BaseRequest.query('64'), bottle.BaseRequest.query('65'), bottle.BaseRequest.query('66'), bottle.BaseRequest.query('67'), bottle.BaseRequest.query('68'), bottle.BaseRequest.query('69')],
-        [bottle.BaseRequest.query('71'), bottle.BaseRequest.query('72'), bottle.BaseRequest.query('73'), bottle.BaseRequest.query('74'), bottle.BaseRequest.query('75'), bottle.BaseRequest.query('76'), bottle.BaseRequest.query('77'), bottle.BaseRequest.query('78'), bottle.BaseRequest.query('79')],
-        [bottle.BaseRequest.query('81'), bottle.BaseRequest.query('82'), bottle.BaseRequest.query('83'), bottle.BaseRequest.query('84'), bottle.BaseRequest.query('85'), bottle.BaseRequest.query('86'), bottle.BaseRequest.query('87'), bottle.BaseRequest.query('88'), bottle.BaseRequest.query('89')],
-        [bottle.BaseRequest.query('91'), bottle.BaseRequest.query('92'), bottle.BaseRequest.query('93'), bottle.BaseRequest.query('94'), bottle.BaseRequest.query('95'), bottle.BaseRequest.query('96'), bottle.BaseRequest.query('97'), bottle.BaseRequest.query('98'), bottle.BaseRequest.query('99')]]
+        [bottle.request.forms['m11'], bottle.request.forms['m12'], bottle.request.forms['m13'], bottle.request.forms['m14'], bottle.request.forms['m15'], bottle.request.forms['m16'], bottle.request.forms['m17'], bottle.request.forms['m18'], bottle.request.forms['m19']],
+        [bottle.request.forms['m21'], bottle.request.forms['m22'], bottle.request.forms['m23'], bottle.request.forms['m24'], bottle.request.forms['m25'], bottle.request.forms['m26'], bottle.request.forms['m27'], bottle.request.forms['m28'], bottle.request.forms['m29']],
+        [bottle.request.forms['m31'], bottle.request.forms['m32'], bottle.request.forms['m33'], bottle.request.forms['m34'], bottle.request.forms['m35'], bottle.request.forms['m36'], bottle.request.forms['m37'], bottle.request.forms['m38'], bottle.request.forms['m39']],
+        [bottle.request.forms['m41'], bottle.request.forms['m42'], bottle.request.forms['m43'], bottle.request.forms['m44'], bottle.request.forms['m45'], bottle.request.forms['m46'], bottle.request.forms['m47'], bottle.request.forms['m48'], bottle.request.forms['m49']],
+        [bottle.request.forms['m51'], bottle.request.forms['m52'], bottle.request.forms['m53'], bottle.request.forms['m54'], bottle.request.forms['m55'], bottle.request.forms['m56'], bottle.request.forms['m57'], bottle.request.forms['m58'], bottle.request.forms['m59']],
+        [bottle.request.forms['m61'], bottle.request.forms['m62'], bottle.request.forms['m63'], bottle.request.forms['m64'], bottle.request.forms['m65'], bottle.request.forms['m66'], bottle.request.forms['m67'], bottle.request.forms['m68'], bottle.request.forms['m69']],
+        [bottle.request.forms['m71'], bottle.request.forms['m72'], bottle.request.forms['m73'], bottle.request.forms['m74'], bottle.request.forms['m75'], bottle.request.forms['m76'], bottle.request.forms['m77'], bottle.request.forms['m78'], bottle.request.forms['m79']],
+        [bottle.request.forms['m81'], bottle.request.forms['m82'], bottle.request.forms['m83'], bottle.request.forms['m84'], bottle.request.forms['m85'], bottle.request.forms['m86'], bottle.request.forms['m87'], bottle.request.forms['m88'], bottle.request.forms['m89']],
+        [bottle.request.forms['m91'], bottle.request.forms['m92'], bottle.request.forms['m93'], bottle.request.forms['m94'], bottle.request.forms['m95'], bottle.request.forms['m96'], bottle.request.forms['m97'], bottle.request.forms['m98'], bottle.request.forms['m99']]]
     #self.matrika = matrika
-    #resen_sudoku = resi()
+    #resen_sudoku = matrika.resi()
     return bottle.template('resen_sudoku.html', celamatrika=matrika)
+
 
 @bottle.post("/matrika/")
 def brisanje_sudokuja():
@@ -35,5 +41,6 @@ def brisanje_sudokuja():
 from bottle import error
 @error(404)
 def error404(error):
-    return 'Ta stran ne obstaja!'
+    return 'Ta stran ne obstaja'
+    
 bottle.run(debug=True, reloader=True)
